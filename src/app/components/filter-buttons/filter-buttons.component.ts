3e1dead7ae7f87c9bc31e093e4122828c27d0020
Xavier Oillic <xavieroillic@gmail.com>
2024-02-02 09:25:48 +0100

    boutons arrosage soleil et alphabetic

--- src/app/components/filter-buttons/filter-buttons.component.ts
+++ src/app/components/filter-buttons/filter-buttons.component.ts
@@ -11,19 +11,19 @@ export class FilterButtonsComponent {
   @Output() sunny_btn = new EventEmitter();
 
   btn_alphabetic(alphabetic: Event) {
-    const target = alphabetic.target as HTMLInputElement;
+    const target = alphabetic.target as HTMLButtonElement;
     this.alphabetic_btn.emit(target.value);
     //console.log('Enfant => ', alphabetic);
   }
 
   btn_arrosage(water: Event) {
-    const target = water.target as HTMLInputElement;
+    const target = water.target as HTMLButtonElement;
     this.water_btn.emit(target.value);
     //console.log('Enfant => ', water);
   }
 
   btn_soleil(sunshine: Event) {
-    const target = sunshine.target as HTMLInputElement;
+    const target = sunshine.target as HTMLButtonElement; // C'est un élément BUTTON HTML que je récupère.
     this.sunny_btn.emit(target.value);
     //console.log('Enfant => ', sunshine);
   }
