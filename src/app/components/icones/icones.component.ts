4a579b47fc85d876e2456608743c4fe4fafe7f83
Xavier Oillic <xavieroillic@gmail.com>
2024-02-21 10:55:34 +0100

    debut de correctif affichage dynmaique des icones

--- src/app/components/icones/icones.component.ts
+++ src/app/components/icones/icones.component.ts
@@ -13,6 +13,13 @@ export class IconesComponent {
   // JE STOCKE DANS UNE PROPRIÉTÉ LA VALEUR DE L'INPUT. QUE J'INITIALISE A RIEN.
   // ET JE LES ENVOIE DANS LE HTML POUR MARQUER LES EMPLACEMENTS
 
-  //@Input() iconName: string = '';
-  //@Input() message: string = '';
+  @Input() iconName:
+    | 'heart'
+    | 'heart-fill'
+    | 'sun'
+    | 'sun-fill'
+    | 'droplet'
+    | 'droplet-fill' = 'heart';
+  // ON TYPE l'IconName avec toutes SES valeurs POTENTIELLES et qu'on lui rend DISPOINBLES.
+  // Si je met une autre valeur que celles déclarées ici, il ne l'acceptera pas.
 }
