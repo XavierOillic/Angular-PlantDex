4291a9cd95bcbcb5f4c6b29f7fcf4334ec13e348
Xavier Oillic <xavieroillic@gmail.com>
2024-02-21 13:36:33 +0100

    icones dynamique

--- src/app/components/icones/icones.component.ts
+++ src/app/components/icones/icones.component.ts
@@ -7,12 +7,6 @@ import { Plant } from 'src/app/models/plant';
   styleUrls: ['./icones.component.css'],
 })
 export class IconesComponent {
-  @Input() sunshineIcons: string = '';
-  @Input() waterIcons: string = '';
-
-  // JE STOCKE DANS UNE PROPRIÉTÉ LA VALEUR DE L'INPUT. QUE J'INITIALISE A RIEN.
-  // ET JE LES ENVOIE DANS LE HTML POUR MARQUER LES EMPLACEMENTS
-
   @Input() iconName:
     | 'heart'
     | 'heart-fill'
@@ -20,6 +14,14 @@ export class IconesComponent {
     | 'sun-fill'
     | 'droplet'
     | 'droplet-fill' = 'heart';
+  @Input() iconColor = 'yellow';
+  @Input() iconSize: number = 2;
+
   // ON TYPE l'IconName avec toutes SES valeurs POTENTIELLES et qu'on lui rend DISPOINBLES.
   // Si je met une autre valeur que celles déclarées ici, il ne l'acceptera pas.
+
+  //@Input() sunshineIcons: string = '';
+  //@Input() waterIcons: string = '';
+  // JE STOCKE DANS UNE PROPRIÉTÉ LA VALEUR DE L'INPUT. QUE J'INITIALISE A RIEN.
+  // ET JE LES ENVOIE DANS LE HTML POUR MARQUER LES EMPLACEMENTS
 }
